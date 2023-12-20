e0d63bfa90992720547724d704dafbee578f4018
Benjamin <benjamin.langlois@devinfluence.fr>
2023-12-20 13:34:33 +0100

    Indentation

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const benjamin = document.getElementById("Ben");
-console.log(benjamin);
-
 benjamin.addEventListener("mouseover", (event) => {
     console.log(benjamin.innerText);
 });
 
 const motHover = document.querySelector('.mot-hover');
-
     motHover.addEventListener('mouseover', function() {
     console.log("Cécile");
     });
+    
 document.addEventListener("DOMContentLoaded", function() {
     var cecile = document.querySelector("#cecile");
     cecile.addEventListener('mouseover', function () {
