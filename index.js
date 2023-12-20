daccc3bb6b0f1e423b2751544a6da678e2d099f9
Mathis TERNON <mathis.ternon@devinfluence.fr>
2023-12-20 13:31:02 +0100

    fix

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const motHover = document.querySelector('.mot-hover');
-
-    motHover.addEventListener('mouseover', function() {
-    motHover.style.color = 'red'; // Change la couleur en rouge au survol
-    console.log("Cécile");
-    });
-
 const benjamin = document.getElementById("Ben");
 console.log(benjamin);
 
 benjamin.addEventListener("mouseover", (event) => {
     console.log(benjamin.innerText);
 });
+
+const motHover = document.querySelector('.mot-hover');
+
+    motHover.addEventListener('mouseover', function() {
+    motHover.style.color = 'red'; // Change la couleur en rouge au survol
+    console.log("Cécile");
+    });
